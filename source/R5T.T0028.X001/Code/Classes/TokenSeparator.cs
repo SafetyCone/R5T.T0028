7957ba0251f5c82d47529cc9a398eac74cf30c25
David Coats <david@getsafetycone.com>
2022-02-14 11:01:31 -0800

    Follow-through on moving functionality from the System to the R5T.Magyar namespace.

--- source/R5T.T0028.X001/Code/Classes/TokenSeparator.cs
+++ source/R5T.T0028.X001/Code/Classes/TokenSeparator.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 
+using R5T.Magyar;
+
 
 namespace R5T.T0028
 {
     public class TokenSeparator
     {
-        public const string Default = Strings.Space;
+        public const string Default = Strings.Space_Const;
 
 
         public static TokenSeparator Instance { get; } = new();
